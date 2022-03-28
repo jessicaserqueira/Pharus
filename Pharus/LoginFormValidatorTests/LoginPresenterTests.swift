23ce4e058ae6ca6fe2e382da8688e2977380167a
VIctor <colen.victor@icloud.com>
2022-03-28 17:36:28 -0300

    tests: add LoginPresenterTests

---
+++ Pharus/LoginFormValidatorTests/LoginPresenterTests.swift
@@ -0,0 +1,96 @@
+//
+//  LoginFormValidatorTests.swift
+//  LoginFormValidatorTests
+//
+//  Created by Victor Colen on 28/03/22.
+//
+
+import XCTest
+@testable import Pharus
+
+class LoginPresenterTests: XCTestCase {
+    
+    func testLoginPresenter_WhenEmailHasConsecutiveDots_ShoudlReturnFalse() {
+        //Arrange
+        let loginPresenter = LoginPresenter()
+        
+        //Act
+        let email = "john@doe........com"
+        let isValidEmail = loginPresenter.isValidEmail(email: email)
+        
+        //Assert
+        XCTAssertFalse(isValidEmail, "Test should have returned false, but returned true instead")
+    }
+    
+    func testLoginPresenter_WhenEmailHasNoAts_shouldReturnFalse() {
+        //Arrange
+        let loginPresenter = LoginPresenter()
+        
+        //Act
+        let email = "johndoe.com"
+        let isValidEmail = loginPresenter.isValidEmail(email: email)
+        
+        //Assert
+        XCTAssertFalse(isValidEmail, "Test should have returned false, but returned true instead")
+    }
+    
+    func testLoginPresenter_WhenEmailHasTwoAts_ShouldReturnFalse() {
+        //Arrange
+        let sut = LoginPresenter()
+        
+        //Act
+        let email = "john@@doe.com"
+        let isValidEmail = sut.isValidEmail(email: email)
+        
+        //Assert
+        XCTAssertFalse(isValidEmail, "Test should have returned false, but returned true instead")
+    }
+    
+    func testLoginPresenter_WhenEmailIsValid_ShouldReturnTrue() {
+        //Arrange
+        let sut = LoginPresenter()
+        
+        //Act
+        let email = "john@doe.com.br"
+        let isValidEmail = sut.isValidEmail(email: email)
+        
+        //Assert
+        XCTAssertTrue(isValidEmail, "Test should have returned true, but returned false instead")
+    }
+    
+    func testLoginPresenter_WhenPasswordCountIsLessThanEight_ShouldReturnFalse() {
+        //Arrange
+        let sut = LoginPresenter()
+        
+        //Act
+        let password = "!234567"
+        let isValidPassword = sut.isValidPassword(password: password)
+        
+        //Assert
+        XCTAssertFalse(isValidPassword, "Test should have returned false, but returned true instead")
+    }
+    
+    func testLoginPresenter_WhenPasswordIsValid_ShouldReturnTrue() {
+        //Arrange
+        let sut = LoginPresenter()
+        
+        //Act
+        let password = "!234567Q"
+        let isValidPassword = sut.isValidPassword(password: password)
+        
+        //Assert
+        XCTAssertTrue(isValidPassword, "Test should have returned true, but returned false instead")
+    }
+    
+    func testLoginPresenter_WhenPasswordCountIsMoreThanEightButHasNoSpecialCharacters_ShouldReturnFalse() {
+        //Arrange
+        let sut = LoginPresenter()
+        
+        //Act
+        let password = "12345678Z"
+        let isValidPassword = sut.isValidPassword(password: password)
+        
+        //Assert
+        XCTAssertFalse(isValidPassword, "Test should have returned false, but returned false true")
+    }
+}
--- Pharus/LoginPresenter.swift
+++ Pharus/LoginPresenter.swift
@@ -14,13 +14,16 @@ protocol LoginPresenterProtocol {
 
 class LoginPresenter: LoginPresenterProtocol {
     func isValidEmail(email: String) -> Bool {
-        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailRegEx = K.RegEx.emailRegEx
 
-        let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
-        return emailPred.evaluate(with: email)
+        let emailPredicate = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailPredicate.evaluate(with: email)
     }
     
     func isValidPassword(password: String) -> Bool {
-        password.count >= 8
+        let passwordRegEx = K.RegEx.passwordRegEx
+        
+        let passwordPredicate = NSPredicate(format: "SELF MATCHES %@", passwordRegEx)
+        return passwordPredicate.evaluate(with: password)
     }
 }
