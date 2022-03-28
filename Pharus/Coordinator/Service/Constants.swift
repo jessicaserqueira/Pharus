0688e66be1c50d35c066af35827c147222f80a49
VIctor <colen.victor@icloud.com>
2022-03-28 16:00:08 -0300

    create constants file

---
+++ Pharus/Coordinator/Service/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Pharus
+//
+//  Created by Victor Colen on 28/03/22.
+//
+
+import Foundation
+
+struct K {
+    struct RegEx {
+        static let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+    }
+}
