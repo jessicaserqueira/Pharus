2c6fa368f774838ac6efa244b006c53dbadba042
VIctor <colen.victor@icloud.com>
2022-03-29 21:30:38 -0300

    change start screen

--- Pharus/View/ViewController/UserProjectsViewController.swift
+++ Pharus/View/ViewController/UserProjectsViewController.swift
@@ -5,4 +5,31 @@
 //  Created by Victor Colen on 29/03/22.
 //
 
-import Foundation
+import UIKit
+
+class UserProjectsViewController: UIViewController {
+    
+    var coordinator: StartFlow?
+  
+       // MARK: - Properties
+       
+    let viewCustom = UIView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        
+    }
+    
+    override func loadView() {
+        super.loadView()
+        
+        let student: Student = Bundle.main.decode("Student.json")
+        print(student)
+        viewCustom.backgroundColor = .red
+        self.view = viewCustom
+    }
+
+    
+}
