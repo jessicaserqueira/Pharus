b69ef9ff2f0bc92007930d7c53d437ab0848f390
VIctor <colen.victor@icloud.com>
2022-04-06 10:11:35 -0300

    feat: add all elements in student project detail view

--- Pharus/View/StudentProjectDetailView.swift
+++ Pharus/View/StudentProjectDetailView.swift
@@ -13,6 +13,7 @@ class StudentProjectDetailView: UIView {
     lazy var mainView: UIView = {
         let view = UIView()
         view.backgroundColor = .cardColor
+        view.layer.cornerRadius = 16
         view.translatesAutoresizingMaskIntoConstraints = false
         view.accessibilityIdentifier = "StudentProjectDetailView.mainView"
         
@@ -32,7 +33,6 @@ class StudentProjectDetailView: UIView {
         let stackView = UIStackView()
         stackView.axis = .vertical
         stackView.spacing = 24
-        stackView.backgroundColor = .green
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.accessibilityIdentifier = "StudentProjectDetailView.mainStackView"
         
@@ -52,7 +52,7 @@ class StudentProjectDetailView: UIView {
     lazy var descriptionStackView: UIStackView = {
         let stackView = UIStackView()
         stackView.axis = .vertical
-        stackView.backgroundColor = .blue
+        stackView.spacing = 16
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.accessibilityIdentifier = "StudentProjectDetailView.descriptionStackView"
         
@@ -83,7 +83,6 @@ class StudentProjectDetailView: UIView {
         let stackView = UIStackView()
         stackView.axis = .vertical
         stackView.spacing = 29
-        stackView.backgroundColor = .yellow
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.accessibilityIdentifier = "StudentProjectDetailView.taskStackView"
         
@@ -92,7 +91,6 @@ class StudentProjectDetailView: UIView {
     
     lazy var titleHelper: UIView = {
         let view = UIView()
-        view.backgroundColor = .red
         view.translatesAutoresizingMaskIntoConstraints = false
         view.accessibilityIdentifier = "StudentProjectDetailView.taskStackView"
         
@@ -114,23 +112,30 @@ class StudentProjectDetailView: UIView {
         stackView.axis = .vertical
         stackView.spacing = 30
         stackView.translatesAutoresizingMaskIntoConstraints = false
-        stackView.backgroundColor = .brown
         stackView.accessibilityIdentifier = "StudentProjectDetailView.individualTaskHelperStackView"
         
         return stackView
     }()
     
-    lazy var checkmarkImageView: UIImageView = {
-        let imageView = UIImageView()
-        imageView.image = UIImage(systemName: "checkmark")
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        imageView.accessibilityIdentifier = "StudentProjectDetailView.checkMarkImageView"
-        return imageView
+    lazy var checkmarkImage: UIImage = {
+        var image = UIImage(systemName: "checkmark")
+        return image!
+    }()
+    
+    lazy var completedTasksProgressStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 16
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.accessibilityIdentifier = "StudentProjectDetailView.completedTasksProgressStackView"
+        return stackView
     }()
     
     lazy var completedTasksLabel: UILabel = {
         let label = UILabel()
-        label.text = "Completadas 3 de 4 tarefas"
+        label.textAlignment = .center
+        label.text = "Completadas 3 de 4 tarefas (75%)"
+        label.numberOfLines = 0
         label.translatesAutoresizingMaskIntoConstraints = false
         label.accessibilityIdentifier = "StudentProjectDetailView.completedTasksLabel"
         
@@ -138,12 +143,31 @@ class StudentProjectDetailView: UIView {
     }()
     
     lazy var completedTasksProgressView: UIProgressView = {
-       let progressView = UIProgressView()
-        progressView.progressTintColor = .blue
+        let progressView = UIProgressView()
+        progressView.progressTintColor = .darkCardColor
         progressView.progress = 0.75
-        progressView.heightAnchor.constraint(equalToConstant: 22).isActive = true
         progressView.layer.cornerRadius = 8
+        progressView.accessibilityIdentifier = "StudentProjectDetailView.completedTasksProgressView"
+        
         return progressView
+    }()
+        
+    lazy var anotherHelperView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .red
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    lazy var uploadFilesButton: UIButton = {
+        let button = UIButton()
+        button.layer.cornerRadius = 16
+        button.setTitle("Enviar arquivos", for: .normal)
+        button.backgroundColor = .darkCardColor
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.accessibilityIdentifier = "StudentProjectDetailView.uploadFilesButton"
+        
+        return button
     }()
     
     override init(frame: CGRect) {
@@ -186,15 +210,13 @@ class StudentProjectDetailView: UIView {
             let taskTitleLabel = UILabel()
             taskTitleLabel.translatesAutoresizingMaskIntoConstraints = false
             taskTitleLabel.text = "Tarefa \(task)"
-            taskTitleLabel.backgroundColor = .white
             
             let taskCheckboxButton = UIButton()
             taskCheckboxButton.layer.borderWidth = 1
             taskCheckboxButton.layer.borderColor = UIColor.black.cgColor
             taskCheckboxButton.layer.cornerRadius = 4
-            taskCheckboxButton.setImage(UIImage(systemName: "checkmark"), for: .normal)
+            taskCheckboxButton.setImage(checkmarkImage, for: .normal)
             
-            taskCheckboxButton.backgroundColor = .systemPink
             
             NSLayoutConstraint.activate([
                 taskCheckboxButton.widthAnchor.constraint(equalToConstant: 25),
@@ -202,12 +224,12 @@ class StudentProjectDetailView: UIView {
             ])
             
             taskCheckboxButton.addAction(UIAction {  _ in
-                if taskCheckboxButton.currentImage == UIImage(systemName: "checkmark") {
+                if taskCheckboxButton.currentImage == self.checkmarkImage {
                     print("desativar")
                     taskCheckboxButton.setImage(.none, for: .normal)
                 } else {
                     print("ativar")
-                    taskCheckboxButton.setImage(UIImage(systemName: "checkmark"), for: .normal)
+                    taskCheckboxButton.setImage(self.checkmarkImage, for: .normal)
                 }
                 
             }, for: .touchUpInside)
@@ -216,11 +238,21 @@ class StudentProjectDetailView: UIView {
             individualTaskStackView.addArrangedSubview(taskTitleLabel)
             
             individualTaskHelperStackView.addArrangedSubview(individualTaskStackView)
-            
-            mainStackView.addArrangedSubview(completedTasksLabel)
-            mainStackView.addArrangedSubview(completedTasksProgressView)
-            
         }
+        
+        mainStackView.addArrangedSubview(completedTasksProgressStackView)
+        
+        completedTasksProgressStackView.addArrangedSubview(completedTasksLabel)
+        completedTasksProgressStackView.addArrangedSubview(completedTasksProgressView)
+        
+        mainStackView.addArrangedSubview(anotherHelperView)
+        
+        anotherHelperView.addSubview(uploadFilesButton)
+        
+        uploadFilesButton.addAction(UIAction { _ in
+            print("enviar arquivos")
+        }, for: .touchUpInside)
+        
     }
     
     //MARK: - Constraints
@@ -242,10 +274,25 @@ class StudentProjectDetailView: UIView {
             mainStackView.centerXAnchor.constraint(equalTo: mainScrollView.centerXAnchor)
         ])
         
+        //Title Helper
         NSLayoutConstraint.activate([
             titleHelper.leadingAnchor.constraint(equalTo: mainStackView.leadingAnchor),
             titleHelper.trailingAnchor.constraint(equalTo: mainStackView.trailingAnchor),
             titleHelper.heightAnchor.constraint(equalToConstant: 30)
+        ])
+        
+        //Completed Tasks Progress View
+        NSLayoutConstraint.activate([
+            completedTasksProgressView.heightAnchor.constraint(equalToConstant: 22)
+        ])
+        
+        //Upload Files Button
+        NSLayoutConstraint.activate([
+            uploadFilesButton.centerYAnchor.constraint(equalTo: anotherHelperView.centerYAnchor),
+            uploadFilesButton.centerXAnchor.constraint(equalTo: anotherHelperView.centerXAnchor),
+            uploadFilesButton.heightAnchor.constraint(equalToConstant: 44),
+            uploadFilesButton.widthAnchor.constraint(equalToConstant: 195),
+            anotherHelperView.heightAnchor.constraint(equalToConstant: 50)
         ])
     }
 }
