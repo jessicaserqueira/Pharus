b5b55d9b0027b254c86e33b847bae7e33b9b926b
VIctor <colen.victor@icloud.com>
2022-04-06 18:07:37 -0300

    merge

---
+++ Pharus/View/Controller/TabBarViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TabBarViewController.swift
+//  Pharus
+//
+//  Created by Jéssica Serqueira on 05/04/22.
+//
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    var coordinator: TabBarFlow?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
