02f307f7125849fb00a60d28948dd6ad522e2f13
VIctor <colen.victor@icloud.com>
2022-04-06 07:33:50 -0300

    bugfix: fix buttons not making part of scrollview

--- Pharus/View/StudentProjectDetailView.swift
+++ Pharus/View/StudentProjectDetailView.swift
@@ -12,7 +12,7 @@ class StudentProjectDetailView: UIView {
     
     lazy var mainView: UIView = {
         let view = UIView()
-        view.backgroundColor = .red
+        view.backgroundColor = .cardColor
         view.translatesAutoresizingMaskIntoConstraints = false
         view.accessibilityIdentifier = "StudentProjectDetailView.mainView"
         return view
@@ -20,7 +20,7 @@ class StudentProjectDetailView: UIView {
     
     lazy var mainScrollView: UIScrollView = {
         var scrollView = UIScrollView()
-        scrollView.backgroundColor = .cardColor
+        scrollView.backgroundColor = .white
         scrollView.translatesAutoresizingMaskIntoConstraints = false
         scrollView.accessibilityIdentifier = "StudentProjectDetailView.mainScrollView"
         return scrollView
@@ -30,6 +30,7 @@ class StudentProjectDetailView: UIView {
         let stackView = UIStackView()
         stackView.axis = .vertical
         stackView.spacing = 24
+        stackView.backgroundColor = .green
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.accessibilityIdentifier = "StudentProjectDetailView.mainStackView"
         return stackView
@@ -38,6 +39,7 @@ class StudentProjectDetailView: UIView {
     lazy var mentorLabel: UILabel = {
         let label = UILabel()
         label.text = "Mentor: José da Silva"
+        label.font = .cardTitleFont
         label.translatesAutoresizingMaskIntoConstraints = false
         label.accessibilityIdentifier = "StudentProjectDetailView.mentorLabel"
         return label
@@ -55,6 +57,7 @@ class StudentProjectDetailView: UIView {
     lazy var descriptionTitleLabel: UILabel = {
         let label = UILabel()
         label.text = "Descrição:"
+        label.font = .cardSubtitleFont
         label.translatesAutoresizingMaskIntoConstraints = false
         label.accessibilityIdentifier = "StudentProjectDetailView.descriptionTitleLabel"
         return label
@@ -69,6 +72,51 @@ class StudentProjectDetailView: UIView {
         return label
     }()
     
+    lazy var taskStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 29
+        stackView.backgroundColor = .yellow
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.accessibilityIdentifier = "StudentProjectDetailView.taskStackView"
+        return stackView
+    }()
+    
+    lazy var titleHelper: UIView = {
+        let view = UIView()
+        view.backgroundColor = .red
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.accessibilityIdentifier = "StudentProjectDetailView.taskStackView"
+        return view
+    }()
+    
+    lazy var taskTitleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Lista de Tarefas"
+        label.font = .cardSubtitleFont
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.accessibilityIdentifier = "StudentProjectDetailView.taskTitleLabel"
+        return label
+    }()
+    
+    lazy var completedTasksLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Completadas 3 de 4 tarefas"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.accessibilityIdentifier = "StudentProjectDetailView.completedTasksLabel"
+        return label
+    }()
+    
+    lazy var individualTaskHelperStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 30
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.backgroundColor = .brown
+        return stackView
+    }()
+    
+    
     override init(frame: CGRect) {
         super.init(frame: .zero)
         
@@ -81,35 +129,77 @@ class StudentProjectDetailView: UIView {
     }
     
     func configureSubviews() {
-        addSubview(mainView)
+        addSubview(mainScrollView)
         
-        mainView.addSubview(mainScrollView)
+        mainScrollView.addSubview(mainView)
         
-        mainScrollView.addSubview(mainStackView)
+        mainView.addSubview(mainStackView)
         
         mainStackView.addArrangedSubview(mentorLabel)
         mainStackView.addArrangedSubview(descriptionStackView)
         
         descriptionStackView.addArrangedSubview(descriptionTitleLabel)
         descriptionStackView.addArrangedSubview(descriptionTextLabel)
+        
+        mainStackView.addArrangedSubview(taskStackView)
+        
+        taskStackView.addArrangedSubview(titleHelper)
+        titleHelper.addSubview(taskTitleLabel)
+        
+        taskStackView.addArrangedSubview(individualTaskHelperStackView)
+        
+        for _ in 1...30 {
+            let individualTaskStackView = UIStackView()
+            individualTaskStackView.translatesAutoresizingMaskIntoConstraints = false
+            individualTaskStackView.axis = .horizontal
+            individualTaskStackView.spacing = 11
+            
+            let taskTitleLabel = UILabel()
+            taskTitleLabel.translatesAutoresizingMaskIntoConstraints = false
+            taskTitleLabel.text = "botao"
+            taskTitleLabel.backgroundColor = .white
+            
+            
+            let taskCheckboxButton = UIButton()
+            taskCheckboxButton.layer.borderWidth = 1
+            taskCheckboxButton.layer.borderColor = UIColor.black.cgColor
+            taskCheckboxButton.layer.cornerRadius = 4
+            taskCheckboxButton.backgroundColor = .systemPink
+            taskCheckboxButton.widthAnchor.constraint(equalToConstant: 25).isActive = true
+            taskCheckboxButton.addAction(UIAction {  _ in
+                print("teste")
+            }, for: .touchUpInside)
+            
+            individualTaskStackView.addArrangedSubview(taskCheckboxButton)
+            individualTaskStackView.addArrangedSubview(taskTitleLabel)
+            
+            individualTaskHelperStackView.addArrangedSubview(individualTaskStackView)
+        }
     }
     
     //MARK: - Constraints
     
     func setupConstraints() {
         
-        //Main View
-        self.stretch(mainView)
-        
         //Scroll View
-        self.stretch(mainScrollView, to: mainView, top: 50, left: 16, right: -16)
+        self.stretch(mainScrollView)
+        
+        //Main View
+        self.stretch(mainView, to: mainScrollView, top: 50, left: 16, bottom: -10, right: -16)
         
         //Main Stack View
         NSLayoutConstraint.activate([
-            mainStackView.topAnchor.constraint(equalTo: mainScrollView.topAnchor, constant: 24),
-            mainStackView.leadingAnchor.constraint(equalTo: mainScrollView.leadingAnchor, constant: 24),
-            mainStackView.trailingAnchor.constraint(equalTo: mainScrollView.trailingAnchor, constant: 24)
+            mainStackView.topAnchor.constraint(equalTo: mainView.topAnchor, constant: 24),
+            mainStackView.leadingAnchor.constraint(equalTo: mainView.leadingAnchor, constant: 24),
+            mainStackView.trailingAnchor.constraint(equalTo: mainView.trailingAnchor, constant: -24),
+            mainStackView.bottomAnchor.constraint(equalTo: mainView.bottomAnchor, constant: -10),
+            mainStackView.centerXAnchor.constraint(equalTo: mainScrollView.centerXAnchor)
+        ])
+        
+        NSLayoutConstraint.activate([
+            titleHelper.leadingAnchor.constraint(equalTo: mainStackView.leadingAnchor),
+            titleHelper.trailingAnchor.constraint(equalTo: mainStackView.trailingAnchor),
+            titleHelper.heightAnchor.constraint(equalToConstant: 30)
         ])
     }
-    
 }
