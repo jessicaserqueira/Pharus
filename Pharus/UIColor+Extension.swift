f95df6f26a639a43bc4d24e1fe0a8148017fd2b3
VIctor <colen.victor@icloud.com>
2022-04-07 19:42:17 -0300

    feat: add app

--- Pharus/UIColor+Extension.swift
+++ Pharus/UIColor+Extension.swift
@@ -10,4 +10,7 @@ import UIKit
 extension UIColor {
     static let cardColor = UIColor(red: 0.882, green: 0.882, blue: 0.882, alpha: 1)
     static let darkCardColor = UIColor(red: 0.769, green: 0.769, blue: 0.769, alpha: 1)
+    
+    static let mainCardButtonBackgroundColor = UIColor(red: 0.765, green: 0.765, blue: 0.765, alpha: 1)
+    static let mainCardButtonTextColor = UIColor(red: 0.153, green: 0.153, blue: 0.153, alpha: 1)
 }
