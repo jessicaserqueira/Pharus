95c4edbc78604937ce390c5bbb353ada3974c2bd
VIctor <colen.victor@icloud.com>
2022-04-16 10:16:31 -0300

    feat: add studentHome

---
+++ Pharus/Scenes/Student Home/HomeMiniCardView.swift
@@ -0,0 +1,112 @@
+//
+//  StudentHomeView.swift
+//  Pharus
+//
+//  Created by Victor Colen on 12/04/22.
+//
+import UIKit
+
+class StudentHomeMiniCardView: UIView {
+    
+    //MARK: - Views
+    
+    lazy var mainView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.accessibilityIdentifier = "StudentHomeMiniCardView.mainView"
+        return view
+    }()
+    
+    lazy var mainStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 15
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.accessibilityIdentifier = "StudentHomeMiniCardView.mainStackView"
+        return stackView
+    }()
+    
+    lazy var cardIconHelperView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.accessibilityIdentifier = "StudentHomeMiniCardView.cardIconHelperView"
+        return view
+    }()
+    
+    lazy var cardIconImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: K.Assets.Images.medalImage)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.accessibilityIdentifier = "StudentHomeMiniCardView.cardIconImageView"
+        return imageView
+    }()
+    
+    lazy var cardDescriptionHelperView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.accessibilityIdentifier = "StudentHomeMiniCardView.cardDescriptionHelperView"
+        return view
+    }()
+    
+    lazy var cardDescriptionLabel: UILabel = {
+        let label = UILabel()
+        label.text = "O projeto ”Introdução a Robótica” te deu uma medalha!"
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.accessibilityIdentifier = "StudentHomeMiniCardView.cardDescriptionLabel"
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        
+        configureSubviews()
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureSubviews() {
+        addSubview(mainView)
+        
+        mainView.addSubview(mainStackView)
+        
+        mainStackView.addArrangedSubview(cardIconHelperView)
+        
+        cardIconHelperView.addSubview(cardIconImageView)
+        
+        mainStackView.addArrangedSubview(cardDescriptionHelperView)
+        
+        cardDescriptionHelperView.addSubview(cardDescriptionLabel)
+    }
+    
+    //MARK: - Constraints
+    
+    func setupConstraints() {
+        //Main View
+        self.stretch(mainView)
+        
+        //Main Stack View
+        self.stretch(mainStackView, to: mainView, top: 10, left: 10, bottom: -10, right: -10)
+        
+        //Card Icon Helper View
+        NSLayoutConstraint.activate([
+            cardIconHelperView.heightAnchor.constraint(equalToConstant: 42)
+        ])
+        
+        //Card Icon Image View
+        cardIconImageView.center(in: cardIconHelperView)
+        
+        //Card Description Label
+        cardDescriptionLabel.center(in: cardDescriptionHelperView)
+        NSLayoutConstraint.activate([
+            cardDescriptionLabel.leadingAnchor.constraint(equalTo: cardDescriptionHelperView.leadingAnchor),
+            cardDescriptionLabel.trailingAnchor.constraint(equalTo: cardDescriptionHelperView.trailingAnchor),
+            cardDescriptionLabel.bottomAnchor.constraint(equalTo: cardDescriptionHelperView.bottomAnchor)
+        ])
+    }
+    
+}
---
+++ Pharus/Scenes/Student Home/StudentHomeViewController.swift
@@ -0,0 +1,48 @@
+//
+//  StudentHomeViewController.swift
+//  Pharus
+//
+//  Created by Victor Colen on 11/04/22.
+//
+import UIKit
+
+class StudentHomeViewController: UIViewController {
+        
+    let customView = StudentHomeView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func loadView() {
+        super.loadView()
+
+        self.view = customView
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        setGradientBackground()
+        super.viewWillAppear(animated)
+    }
+    
+    func setGradientBackground() {
+       let colors = [
+          UIColor(red: 0.235, green: 0.125, blue: 0.694, alpha: 1).cgColor,
+          UIColor(red: 0.933, green: 0.298, blue: 0.557, alpha: 1).cgColor,
+          UIColor(red: 0.918, green: 0.525, blue: 0.259, alpha: 1).cgColor
+        ]
+        
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = colors
+        
+        gradientLayer.frame = self.view.bounds
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0.4)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 1.2)
+        
+        gradientLayer.bounds = view.bounds.insetBy(dx: -0.6 * view.bounds.size.width,
+                                                   dy: -0.5 * view.bounds.size.height)
+                
+        self.view.layer.insertSublayer(gradientLayer, at: 0)
+    }
+}
