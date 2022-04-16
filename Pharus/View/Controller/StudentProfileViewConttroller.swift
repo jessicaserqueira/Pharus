b2b86d811c9aed74a44cca21f069dbcb35b962ef
Jessica <jessica.serqueira7@gmail.com>
2022-04-16 09:31:52 -0300

    creating canvas element

--- Pharus/View/Controller/StudentProfileViewConttroller.swift
+++ Pharus/View/Controller/StudentProfileViewConttroller.swift
@@ -8,25 +8,51 @@
 import UIKit
 
 class StudentProfileViewController: UIViewController {
+    
+    private lazy var customView = StudentProfileView()
     var coordinator: StudentProfileFlow?
+    var presenter: StudentProfilePresenter?
+    var student: Student?
     
     // MARK: - Properties
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view.
+        setNavigationBar()
         
     }
     
     override func loadView() {
         super.loadView()
         
+        
+        self.view = customView
+        
         view.backgroundColor = .white
-//        let student: Student = Bundle.main.decode("Student.json")
-//        print(student)
-        //        viewCustom.backgroundColor = .cyan
-        //        self.view = viewCustom
+        
+    }
+    
+    func setNavigationBar() {
+        
+        self.title = "Perfil"
+        var backButtonImage = UIImage(named: K.AssetsNames.backButton)
+        backButtonImage = backButtonImage?.withTintColor(UIColor(red: 0.153,
+                                                                 green: 0.153,
+                                                                 blue: 0.153,
+                                                                 alpha: 1),
+                                                         renderingMode: .alwaysOriginal)
+        
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(image: backButtonImage,
+                                                                style: .plain,
+                                                                target: self,
+                                                                action: #selector(backButtonPressed))
+        
+        
+    }
+    //Implementar quando o fluxo estiver pronto
+    @objc func backButtonPressed() {
+        print("Back button pressed")
     }
 }
+
--- Pharus/View/StudentProfileView.swift
+++ Pharus/View/StudentProfileView.swift
@@ -16,16 +16,199 @@ class StudentProfileView: UIView {
         
         return view
     }()
-
+    
     lazy var mainStackView: UIStackView = {
         let stackView = UIStackView()
         stackView.axis = .vertical
-        stackView.spacing = UIScreen.main.bounds.height/17
-        stackView.alignment = .fill
+        stackView.spacing = 32
+        stackView.distribution = .fillProportionally
         stackView.translatesAutoresizingMaskIntoConstraints = false
-        stackView.accessibilityIdentifier = "LoginView.mainStackView"
+        stackView.accessibilityIdentifier = "StudentProfileView.mainStackView"
         
         return stackView
+    }()
+    
+    lazy var stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 32
+        stackView.distribution = .fillProportionally
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.accessibilityIdentifier = "StudentProfileView.stackView"
+        
+        return stackView
+    }()
+    
+    lazy var userImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named:"profile.fill")
+        imageView.frame = CGRect(x: 0, y: 0, width: 120, height: 120)
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.accessibilityIdentifier = "StudentProfileView.userImageView"
+        
+        return imageView
+    }()
+    
+    lazy var nameStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 12
+        stackView.distribution = .fillProportionally
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.accessibilityIdentifier = "StudentProfileView.nameStackView"
+        
+        return stackView
+    }()
+    
+    lazy var nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Nome"
+        label.font = .boldSystemFont(ofSize: 18)
+        label.textColor = .black
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.accessibilityIdentifier = "StudentProfileView.nameLabel"
+        
+        return label
+    }()
+    
+    lazy var nameTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Antonia Ferreira"
+        textField.font = .systemFont(ofSize: 14)
+        textField.autocapitalizationType = .none
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.accessibilityIdentifier = "StudentProfileView.nameTextField"
+        
+        return textField
+    }()
+    
+    lazy var emailStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 12
+        stackView.distribution = .fillProportionally
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.accessibilityIdentifier = "StudentProfileView.emailStackView"
+    
+        return stackView
+    }()
+    
+    lazy var emailLabel: UILabel = {
+        let label = UILabel()
+        label.text = "E-mail"
+        label.font = .boldSystemFont(ofSize: 18)
+        label.textColor = .black
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.accessibilityIdentifier = "StudentProfileView.emailLabel"
+        
+        return label
+    }()
+    
+    lazy var emailTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "antonia.ferreira@gmail.com"
+        textField.font = .systemFont(ofSize: 14)
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.accessibilityIdentifier = "StudentProfileView.emailTextField"
+        
+        return textField
+    }()
+    
+    lazy var schoolStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 12
+        stackView.distribution = .fillProportionally
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.accessibilityIdentifier = "StudentProfileView.schoolStackView"
+    
+        return stackView
+    }()
+    
+    lazy var schoolLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Escola"
+        label.font = .boldSystemFont(ofSize: 18)
+        label.textColor = .black
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.accessibilityIdentifier = "StudentProfileView.schoolLabel"
+        
+        return label
+    }()
+    
+    lazy var schoolTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Escola Estadual João da Silva"
+        textField.font = .systemFont(ofSize: 14)
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.accessibilityIdentifier = "StudentProfileView.schoolTextField"
+        
+        return textField
+    }()
+    
+    lazy var schoolYearStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 12
+        stackView.distribution = .fillProportionally
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.accessibilityIdentifier = "StudentProfileView.schoolYearStackView"
+    
+        return stackView
+    }()
+    
+    lazy var schoolYearLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Período Escolar"
+        label.font = .boldSystemFont(ofSize: 18)
+        label.textColor = .black
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.accessibilityIdentifier = "StudentProfileView.schoolYearLabel"
+        
+        return label
+    }()
+    
+    lazy var schoolYearTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "7° ano"
+        textField.font = .systemFont(ofSize: 14)
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.accessibilityIdentifier = "StudentProfileView.schoolYearTextField"
+        
+        return textField
+    }()
+    
+    lazy var cityStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 12
+        stackView.distribution = .fillProportionally
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.accessibilityIdentifier = "StudentProfileView.cityStackView"
+    
+        return stackView
+    }()
+    
+    lazy var cityLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Cidade"
+        label.font = .boldSystemFont(ofSize: 18)
+        label.textColor = .black
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.accessibilityIdentifier = "StudentProfileView.cityLabel"
+        
+        return label
+    }()
+    
+    lazy var cityTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Lavras, MG"
+        textField.font = .systemFont(ofSize: 14)
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.accessibilityIdentifier = "StudentProfileView.cityTextField"
+        
+        return textField
     }()
 
     override init(frame: CGRect) {
@@ -43,6 +226,34 @@ class StudentProfileView: UIView {
         addSubview(mainView)
         
         mainView.addSubview(mainStackView)
+        mainStackView.addSubview(stackView)
+        
+        stackView.addArrangedSubview(userImageView)
+        
+        
+        stackView.addArrangedSubview(nameStackView)
+        nameStackView.addArrangedSubview(nameLabel)
+        nameStackView.addArrangedSubview(nameTextField)
+        
+        stackView.addArrangedSubview(emailStackView)
+        emailStackView.addArrangedSubview(emailLabel)
+        emailStackView.addArrangedSubview(emailTextField)
+        
+        stackView.addArrangedSubview(schoolStackView)
+        schoolStackView.addArrangedSubview(schoolLabel)
+        schoolStackView.addArrangedSubview(schoolTextField)
+        
+        stackView.addArrangedSubview(schoolStackView)
+        schoolStackView.addArrangedSubview(schoolLabel)
+        schoolStackView.addArrangedSubview(schoolTextField)
+        
+        stackView.addArrangedSubview(schoolYearStackView)
+        schoolYearStackView.addArrangedSubview(schoolYearLabel)
+        schoolYearStackView.addArrangedSubview(schoolYearTextField)
+        
+        stackView.addArrangedSubview(cityStackView)
+        cityStackView.addArrangedSubview(cityLabel)
+        cityStackView.addArrangedSubview(cityTextField)
         
     }
     
@@ -50,7 +261,30 @@ class StudentProfileView: UIView {
         
         //Main View
         self.stretch(mainView)
-       
-    }
-    
+        
+ 
+        //Main Stack View
+        NSLayoutConstraint.activate([
+            mainStackView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            mainStackView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            mainStackView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            mainStackView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            mainStackView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor)
+        ])
+        
+        //Stack View
+        NSLayoutConstraint.activate([
+            stackView.centerXAnchor.constraint(equalTo: mainView.centerXAnchor),
+            stackView.trailingAnchor.constraint(equalTo: mainView.trailingAnchor, constant: -30),
+            stackView.leadingAnchor.constraint(equalTo: mainView.leadingAnchor, constant: 30),
+
+        ])
+        
+        //User Image View
+        NSLayoutConstraint.activate([
+            userImageView.centerXAnchor.constraint(equalTo: mainStackView.centerXAnchor),
+            userImageView.heightAnchor.constraint(equalToConstant: 150)
+           
+        ])
+}
 }
