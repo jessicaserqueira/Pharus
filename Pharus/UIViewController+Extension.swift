7f9470ba46f59272435923b95f143521dc089fe9
Jessica <jessica.serqueira7@gmail.com>
2022-04-22 20:21:36 -0300

    refactor gradient Background

--- Pharus/UIViewController+Extension.swift
+++ Pharus/UIViewController+Extension.swift
@@ -12,8 +12,8 @@ extension UIViewController {
     
     func setGradientBackground() {
         let colors = [
-            UIColor(red: 0.235, green: 0.125, blue: 0.694, alpha: 1).cgColor,
-            UIColor(red: 0.933, green: 0.298, blue: 0.557, alpha: 1).cgColor,
+            UIColor(red: 0.867, green: 0.149, blue: 0.208, alpha: 1).cgColor,
+            UIColor(red: 0.42, green: 0.247, blue: 0.682, alpha: 1).cgColor,
             UIColor(red: 0.918, green: 0.525, blue: 0.259, alpha: 1).cgColor
         ]
         
@@ -21,8 +21,8 @@ extension UIViewController {
         gradientLayer.colors = colors
         
         gradientLayer.frame = self.view.bounds
-        gradientLayer.startPoint = CGPoint(x: 0, y: 0.4)
-        gradientLayer.endPoint = CGPoint(x: 1, y: 1.2)
+        gradientLayer.startPoint = CGPoint(x: 0.5, y: 0.1)
+        gradientLayer.endPoint = CGPoint(x: 0.8, y: 1.4)
         
         gradientLayer.bounds = view.bounds.insetBy(dx: -0.6 * view.bounds.size.width,
                                                    dy: -0.5 * view.bounds.size.height)
